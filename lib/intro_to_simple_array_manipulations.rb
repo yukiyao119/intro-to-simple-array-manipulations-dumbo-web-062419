a26b2ad7f4cec493369eb9c081088d30ecedf830
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-30 19:57:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,14 +39,8 @@ def using_flatten(arr)
   arr.flatten
 end
 
-
-# describe "#using_flatten" do 
-#   it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
-#     private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-#     flat_array = using_flatten(private_colleges_in_newyork)
-#     expect(flat_array).to eq(["New York University", "Manhattan School of Music ", "Columbia University", "The Juilliard School", "Bard College", "Cooper Union"])
-#   end
-# end
+def using_delete(arr, str)
+  arr.delete
 
 
 # describe "#using_delete" do 
