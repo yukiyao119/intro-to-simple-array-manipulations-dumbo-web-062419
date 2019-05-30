c27587c4724f2133974f1c78d35ada2e5e96327e
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-30 19:50:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,21 +31,10 @@ def using_insert(arr, ele)
   arr.insert(4, ele)
 end
 
-# describe "#using_insert" do 
-#   before(:each) do 
-#     @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-#     @another_esoteric_language = "Malbolge"
-#     @new_array = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
-#   end
+def using_uniq(arr)
+  arr.uniq()
+end
 
-#   it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-#     expect(@new_array[4]).to eq(@another_esoteric_language)
-#   end
-
-#   it "increases the length of the array" do 
-#     expect(@new_array.length).to eq(8)
-#   end
-# end
 
 # describe "#using_uniq" do 
 #   it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
