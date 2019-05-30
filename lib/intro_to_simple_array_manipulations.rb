0ef82cff3831c63707b414aa0e4835ba76160fc6
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-30 19:31:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,20 +8,7 @@ end
 
 
 # describe "#using_unshift" do 
-#   before(:each) do 
-#     neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-#     @new_neighborhood = "Brooklyn Heights"
-#     @updated_array = using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
-#   end
-  
-#   it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
-#     expect(@updated_array.first).to eq(@new_neighborhood)
-#   end
 
-#   it "increases the length of the array" do 
-#     expect(@updated_array.size).to eq(6)
-#   end
-# end
 
 # describe "#using_pop" do
 #   before(:each) do 
